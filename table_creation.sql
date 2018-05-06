a229356629cf60b555039de3060c2aac280371f3
Claire Mudge <ztnvudg@ad.ucl.ac.uk>
2018-05-06 23:19:49 +0100

    Adding table creation code

---
+++ table_creation.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE questions (
+  question VARCHAR(300) NOT NULL,
+  answer1 VARCHAR(300) NOT NULL,
+  answer2 VARCHAR(300) NOT NULL,
+  answer3 VARCHAR(300) NOT NULL,
+  answer4 VARCHAR(300) NOT NULL,
+  correctAnswer INT NOT NULL);
+
+SELECT AddGeometryColumn('public', 'questions', 'geom', 48, 'geometry', 2);
+
+
+  
